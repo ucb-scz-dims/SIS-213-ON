9ca5d13cf278f49a9a8ba95f64b5233691141224
Enrique Diaz <diazenrive@gmail.com>
2025-03-28 02:22:12 -0400

    crear tarjetas de restaurantes y su redireccion

--- OrderNow-React/src/App.jsx
+++ OrderNow-React/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Home'
-import Restaurantes from './Restaurantes'
+import Home from './pages/Home'
+import Restaurantes from './pages/Restaurantes'
+import Restaurante from './pages/Restaurante';
 import './App.css'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/restaurantes" element={<Restaurantes />} />
+        <Route path="/restaurante/:id" element={<Restaurante />} />
       </Routes>
     </Router>
   )
--- OrderNow-React/src/pages/Home.jsx
+++ OrderNow-React/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import BotonRestaurantes from './BotonRestaurantes'; // Asegúrate de que la ruta sea correcta
+import BotonRestaurantes from '../components/BotonRestaurantes'; // Asegúrate de que la ruta sea correcta
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center  h-screen ">
       <h1 className="text-4xl font-bold mb-6">HOME PAGE</h1>
       <BotonRestaurantes />
     </div>
