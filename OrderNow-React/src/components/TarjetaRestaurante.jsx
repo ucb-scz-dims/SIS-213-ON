833307c57c1287d50738f9018cf587578aff615e
Enrique Diaz <diazenrive@gmail.com>
2025-03-30 02:25:13 -0400

    change cards

--- OrderNow-React/src/components/TarjetaRestaurante.jsx
+++ OrderNow-React/src/components/TarjetaRestaurante.jsx
@@ -11,9 +11,9 @@ const TarjetaRestaurante = ({ id, nombre, descripcion, estrellas, comidas }) => 
   return (
     <div 
       onClick={handleClick}
-      className="border border-gray-300 rounded-xl p-4 shadow-md w-100 cursor-pointer hover:shadow-lg transition-shadow mb-4"
+      className="border border-gray-300 rounded-xl p-4 shadow-md flex flex-col md:flex-row cursor-pointer hover:shadow-lg transition-shadow mb-4"
     >
-      <div className="flex items-center mb-4">
+      <div className="flex items-center mb-4 md:mb-0 md:mr-4">
         <div className="bg-purple-600 rounded-full w-12 h-12 mr-4"></div>
         <div className="text-left">
           <h2 className="text-xl font-bold">{nombre}</h2>
@@ -23,7 +23,7 @@ const TarjetaRestaurante = ({ id, nombre, descripcion, estrellas, comidas }) => 
       </div>
       
       {/* Contenedor para las comidas recomendadas con desplazamiento funcional */}
-      <div className="overflow-x-auto whitespace-nowrap py-2 mb-4 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+      <div className="overflow-x-auto whitespace-nowrap w-100 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 md:ml-2">
         <div className="flex space-x-4">
           {comidas.map((comida, index) => (
             <div key={index} className="flex flex-col items-center min-w-max">
--- OrderNow-React/src/pages/Home.jsx
+++ OrderNow-React/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import BotonRestaurantes from '../components/BotonRestaurantes';
 
 const Home = () => {
   return (
-      <div className="flex flex-col items-center  h-screen ">
+      <div className="flex flex-col items-center ">
         <h1 className="text-4xl font-bold mb-6">HOME PAGE</h1>
         <BotonRestaurantes />
       </div>
