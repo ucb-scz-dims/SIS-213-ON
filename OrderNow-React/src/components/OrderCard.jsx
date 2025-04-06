4c5164dd3331a52aa6869de2f070d20c82103d6c
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2025-04-06 18:11:50 -0400

    mejoras en las tarjetas de orden y carrito
    todavía se debe implementar supabase :c

--- OrderNow-React/src/components/OrderCard.jsx
+++ OrderNow-React/src/components/OrderCard.jsx
@@ -1,11 +1,38 @@
-import friedChickenImage from "../assets/images/pollo.jfif";
+import { useState } from "react";
 
-function OrderCard(props) {
+function OrderCard({
+  id,
+  srcImage,
+  title,
+  description,
+  price,
+  onIncrease,
+  onDecrease,
+  onDelete,
+}) {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncrease = () => {
+    const newQuantity = quantity + 1;
+    setQuantity(newQuantity);
+
+    onIncrease(price);
+  };
+
+  const handleDecrease = () => {
+    const newQuantity = quantity - 1;
+    setQuantity(newQuantity);
+
+    onDecrease(newQuantity, price, id);
+  };
+  const handleDelete = () => {
+    onDelete(quantity, price, id);
+  }
   return (
     <div className="flex flex-row items-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 w-full max-w-6xl h-[168px] overflow-hidden mx-auto mt-1">
       {/* Imagen del producto */}
-      <div>
-        <img className="w-1/1" src={friedChickenImage} alt="Product" />
+      <div className="object-cover max-w-2/9">
+        <img className="w-1/1" src={srcImage} alt="Product" />
         <p></p>
       </div>
 
@@ -13,37 +40,52 @@ function OrderCard(props) {
       <div className="p-5 w-2/3 h-full flex">
         <div className="flex-col flex justify-around">
           <h5 className="text-lg font-bold text-gray-900 dark:text-white">
-            Pollo
+            {title}
           </h5>{" "}
           {/*Nombre del producto*/}
           <p className="text-sm text-gray-700 dark:text-gray-400 text-ellipsis">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
-            distinctio!
+            {description}
           </p>{" "}
           {/*Descripcion del producto*/}
           <span className="text-lg font-semibold text-gray-900 dark:text-white">
-            Bs. 20
+            Bs. {price * quantity}
           </span>
           {/*Precio*/}
         </div>
       </div>
 
       {/* Sección de precio y cantidad */}
-      <div className="flex items-center py-4 flex-col">
-        <a className="p-1 text-lg font-bold text-gray-900 dark:text-white">Cantidad</a>
-        <div className="flex items-center space-x-2">
-          <button className=" bg-black text-white rounded-lg hover:bg-red-800 w-8 h-8">
-            -
-          </button>
-          <span className="text-lg font-medium text-gray-900 dark:text-white">
-            1
-          </span>{" "}
-          {/*Cantidad*/}
-          <button className=" bg-black text-white rounded-lg hover:bg-green-800 w-8 h-8">
-            +
-          </button>
+      <div className="flex items-center py-4 h-full flex-col justify-around">
+        <div className="flex flex-col">
+          <a className="p-1 text-lg font-bold text-gray-900 dark:text-white">
+            Cantidad
+          </a>
+          <div className="flex items-center space-x-2">
+            <button
+              className=" bg-black text-white rounded-lg enabled:hover:bg-red-500 w-8 h-8 disabled:cursor-not-allowed"
+              onClick={handleDecrease}
+              disabled={quantity < 2 ? true : false}
+            >
+              -
+            </button>
+            <span className="text-lg font-medium text-gray-900 dark:text-white">
+              {quantity}
+            </span>{" "}
+            {/*Cantidad*/}
+            <button
+              className=" bg-black text-white rounded-lg hover:bg-green-500 w-8 h-8"
+              onClick={handleIncrease}
+            >
+              +
+            </button>
+          </div>
         </div>
-        <a className="p-1 text-gray-900 dark:text-gray-600">Eliminar</a>
+        <a
+          className="p-1 text-gray-900 dark:text-gray-400 hover:text-red-500 cursor-pointer"
+          onClick={handleDelete}
+        >
+          Eliminar
+        </a>
       </div>
     </div>
   );
--- OrderNow-React/src/pages/Cart.jsx
+++ OrderNow-React/src/pages/Cart.jsx
@@ -2,21 +2,104 @@ import OrderCard from "../components/OrderCard";
 import Button from "../components/button/button";
 import { useNavigate } from "react-router-dom";
 import React from "react";
+import { useState } from "react";
 
+//! SOLO UTILIZAR ENLACES DE IMÁGENES, NO USAR FOTOS LOCALES!!!!
 
+const fakeProducts = [
+  {
+    id: 1,
+    srcImage: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Roast_chicken.jpg",
+    title: "Pollos Campeón",
+    description:
+      "Pollo Campeón es un restaurante que sirve pollo a la brasa y es conocido por su salsa picante.",
+    price: 10,
+  },
+  {
+    id: 2,
+    srcImage: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Roast_chicken.jpg",
+    title: "Pollos Campeón",
+    description:
+      "Pollo Campeón es un restaurante que sirve pollo a la brasa y es conocido por su salsa picante.",
+    price: 20,
+  },
+  {
+    id: 3,
+    srcImage: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Roast_chicken.jpg",
+    title: "Pollos Campeón",
+    description:
+      "Pollo Campeón es un restaurante que sirve pollo a la brasa y es conocido por su salsa picante.",
+    price: 30,
+  },
+];
+
+let totalPrice = fakeProducts.reduce(
+  (partialSum, { price }) => partialSum + price,
+  0
+);
 
 function Cart() {
+  const [products, setProducts] = useState(fakeProducts);
+  const [price, setPrice] = useState(totalPrice);
+
+  const onIncrease = (productPrice) => {
+    const newPrice = price + productPrice;
+    setPrice(newPrice);
+  };
+
+  const onDecrease = (quantity, productPrice, productId) => {
+    if (quantity <= 0) {
+      const update = products.filter((product) => product.id !== productId);
+      setProducts(update);
+    }
+
+    const newPrice = price - productPrice;
+    setPrice(newPrice);
+  };
+
+  const onDelete = (quantity, productPrice, productId) => {
+    const update = products.filter((product) => product.id !== productId);
+    setProducts(update);
+
+    const newPrice = price - productPrice * quantity;
+    setPrice(newPrice);
+  }
+
   const navigate = useNavigate();
-const handleClick = () => {
-  navigate(`/restaurantes`);
-};
+  const restaurant = () => {
+    navigate(`/restaurantes`);
+  };
+
+  if(price <= 0){
+    return (
+      <div className="space-y-4 flex flex-col items-center mt-3 pt-24">
+        <a>No tienes ningún producto en el carrito.</a>
+        <div onClick={restaurant}>
+          <Button label="Pedir productos" type="button" />
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="space-y-4 flex flex-col items-center mt-3 pt-24">
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
-      <div onClick={handleClick}>
-      <Button label="Continuar con el pedido" type="button"/>
+      {products.map((product) => (
+        <OrderCard
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          srcImage={product.srcImage}
+          price={product.price}
+          onIncrease={onIncrease}
+          onDecrease={onDecrease}
+          onDelete={onDelete}
+        />
+      ))}
+      <div className="flex flex-col items-center">
+        <h3 className="text-xl m-2">Bs. {price}</h3>
+        <div onClick={restaurant}>
+          <Button label="Continuar con el pedido" type="button" />
+        </div>
       </div>
     </div>
   );
