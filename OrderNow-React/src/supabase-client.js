9b4c8d9afaabeec4fe60d560ee5b042208e2f4d5
Fernando Capobianco <fernandoimrecapobiancotorok@gmail.com>
2025-04-06 20:38:54 -0400

    Actualizacion del cliente SupaBase, del schema api a com

--- OrderNow-React/src/supabase-client.js
+++ OrderNow-React/src/supabase-client.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey, { db: { schema: 'api' } });
+const supabase = createClient(supabaseUrl, supabaseKey, { db: { schema: 'com' } });
 
 export default supabase;
