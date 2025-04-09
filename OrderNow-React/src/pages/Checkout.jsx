b0289cdfa6f6f5917fd1f0d9e911da96541a4ce5
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2025-04-09 11:23:27 -0400

    cambios en el checkout
    ahora es una tabla apropiada, aún falta hacer GET y POST

--- OrderNow-React/src/pages/Checkout.jsx
+++ OrderNow-React/src/pages/Checkout.jsx
@@ -1,14 +1,12 @@
-import OrderCard from "../components/OrderCard";
 import Button from "../components/button/button";
 import { useNavigate } from "react-router-dom";
 import React from "react";
 import { useState } from "react";
+import CheckoutElement from "../components/checkout-element/checkout-element";
 
 const fakeProducts = [
   {
     id: 1,
-    srcImage:
-      "https://upload.wikimedia.org/wikipedia/commons/a/a5/Roast_chicken.jpg",
     title: "Pollos Campeón 1/4",
     description:
       "Pollo Campeón es un restaurante que sirve pollo a la brasa y es conocido por su salsa picante.",
@@ -17,8 +15,6 @@ const fakeProducts = [
   },
   {
     id: 2,
-    srcImage:
-      "https://upload.wikimedia.org/wikipedia/commons/a/a5/Roast_chicken.jpg",
     title: "Pollos Campeón 1/2",
     description:
       "Pollo Campeón es un restaurante que sirve pollo a la brasa y es conocido por su salsa picante.",
@@ -27,8 +23,6 @@ const fakeProducts = [
   },
   {
     id: 3,
-    srcImage:
-      "https://upload.wikimedia.org/wikipedia/commons/a/a5/Roast_chicken.jpg",
     title: "Pollos Campeón Nuggets",
     description:
       "Pollo Campeón es un restaurante que sirve pollo a la brasa y es conocido por su salsa picante.",
@@ -38,7 +32,7 @@ const fakeProducts = [
 ];
 
 let totalPrice = fakeProducts.reduce(
-  (partialSum, { price, quantity }) => partialSum + (price*quantity),
+  (partialSum, { price, quantity }) => partialSum + price * quantity,
   0
 );
 
@@ -51,17 +45,34 @@ function Checkout() {
   };
   return (
     <div className="space-y-4 flex flex-col items-center mt-3 pt-24">
-      {products.map((product) => (
-        <OrderCard
-          key={product.id}
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          srcImage={product.srcImage}
-          price={product.price}
-          quantity={product.quantity}
-        />
-      ))}
+      <h1>Confirmación del producto</h1>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <tr className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <th class="px-6 py-3">Producto</th>
+            <th class="px-6 py-3">Cantidad</th>
+            <th class="px-6 py-3">Precio unit.</th>
+            <th class="px-6 py-3">Precio total</th>
+          </tr>
+
+          {products.map((product) => (
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+              <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {product.title}
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {product.quantity}
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {product.price}
+              </td>
+              <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {product.price * product.quantity}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
       <div className="flex flex-col items-center">
         <h3 className="text-xl m-2">Total del pedido: {price} Bs.</h3>
         <div onClick={restaurant}>
