92770a6f314230a15e9a057f6cf3056be7f01a7a
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-04-10 12:44:33 -0400

    Refactorización: Conexión con supabase y mejora de UX
    1. Se añadió una carpeta para la obtención del cliente de supabase para separar responsabilidades.
    2. Se añadieron estados de carga y error para tener una mejor experiencia de usuario

--- OrderNow-Dashboard/src/components/order-detail/OrderDetail.jsx
+++ OrderNow-Dashboard/src/components/order-detail/OrderDetail.jsx
@@ -1,21 +1,45 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-import CloseIcon from "../../subcomponents/icons/CloseIcon"
+import getSupaBaseClient from "../../supabase/supabase-client";
+import CloseIcon from "../../subcomponents/icons/CloseIcon";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-function OrderDetail({orderId, closeModal}) {
+function OrderDetail({ orderId, closeModal }) {
   const [orderDetail, setOrderDetail] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const supaBaseCom = getSupaBaseClient("com");
 
-  const supaBaseCom = createClient(supabaseUrl, supabaseKey, {
-    db: { schema: "com" },
-  });
+  let content;
 
   useEffect(() => {
     fetchBusiness();
   }, []);
+
+  if (loading) {
+    content = <p>Cargando detalle del pedido..</p>;
+  } else if (error) {
+    content = <p>Error en la consulta: {error.message}</p>;
+  } else {
+    content = (
+      <div className="flex flex-col mb-5">
+        <h3 className="text-2xl text-center font-semibold">Productos</h3>
+
+        <div className="flex flex-col gap-3">
+          {orderDetail.order_details?.map((order_detail) => (
+            <div
+              className="flex flex-row justify-between"
+              key={order_detail.id}
+            >
+              <p>
+                x{order_detail.quantity} {order_detail.products.name}
+              </p>
+              <p>Bs. {order_detail.products.price * order_detail.quantity}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 
   const fetchBusiness = async () => {
     const { data, error } = await supaBaseCom
@@ -37,14 +61,13 @@ function OrderDetail({orderId, closeModal}) {
       .single();
 
     if (error) {
-      alert(error.message);
+      setError(error.message);
       return;
     }
-    
+
     setOrderDetail(data);
+    setLoading(false);
   };
-
-
 
   return (
     <>
@@ -56,7 +79,9 @@ function OrderDetail({orderId, closeModal}) {
         <div className="flex justify-center items-center h-full">
           <div className="max-w-lg w-full m-3 h-[70%] bg-white border border-gray-200 shadow-2xs rounded-md">
             <div className="flex justify-between items-center py-3 px-4 border-b  h-[10%]">
-              <h3 className="font-semibold text-gray-800 text-2xl">Detalle del Pedido</h3>
+              <h3 className="font-semibold text-gray-800 text-2xl">
+                Detalle del Pedido
+              </h3>
               <button
                 type="button"
                 className="inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-hidden focus:bg-gray-200"
@@ -67,33 +92,11 @@ function OrderDetail({orderId, closeModal}) {
             </div>
 
             <div className="p-4 overflow-y-auto h-[80%] flex flex-col">
-              <div className="flex flex-col mb-5">
-                <h3 className="text-2xl text-center font-semibold">Productos</h3>
-
-                <div className="flex flex-col gap-3">
-                  {orderDetail.order_details?.map((order_detail) => (
-                    <div
-                      className="flex flex-row justify-between"
-                      key={order_detail.id}
-                    >
-                      <p>
-                        x{order_detail.quantity} {order_detail.products.name}
-                      </p>
-                      <p>
-                        Bs. {order_detail.products.price * order_detail.quantity}
-                      </p>
-                    </div>
-                  ))}
-
-                </div>
-
-                
-              </div>
-
+              {content}
               <div className="flex flex-row justify-between border-t mt-auto">
-                  <p>Total: </p>
-                  <p>Bs. {orderDetail.total_price}</p>
-                </div>
+                <p>Total: </p>
+                <p>Bs. {orderDetail.total_price}</p>
+              </div>
             </div>
 
             <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t h-[10%]">
@@ -114,7 +117,6 @@ function OrderDetail({orderId, closeModal}) {
           </div>
         </div>
       </div>
-    
     </>
   );
 }
