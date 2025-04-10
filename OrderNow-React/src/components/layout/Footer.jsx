3667c9a6e1204884afdb38ca3dd81545b43f122a
RaidenMakio <rmakio96@gmail.com>
2025-04-10 00:22:21 -0400

    Se hacen arreglos varios desde el navbar hasta el footer en App

--- OrderNow-React/src/components/layout/Footer.jsx
+++ OrderNow-React/src/components/layout/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-800 fixed bottom-0 left-0 right-0 ">
-      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-800 bottom-0 left-0 right-0 ">
+      <div className="w-full mx-auto max-w-screen-xl p-2 md:flex md:items-center md:justify-center">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2024 <a href="/" className="hover:underline">OrderNow</a>. All Rights Reserved.
+          © 2025 <a href="/" className="hover:underline">OrderNow</a>. Todos los derechos reservados.
         </span>
       </div>
     </footer>
