74ee72478690da39fea36b73a5ff0d8e5cd6e3be
JeanPaulCabreraFlores <126268240+lomandopuntocom@users.noreply.github.com>
2025-04-23 16:13:05 -0400

    Eliminado Else

--- OrderNow-Dashboard/src/pages/OrderDashboard.jsx
+++ OrderNow-Dashboard/src/pages/OrderDashboard.jsx
@@ -48,27 +48,33 @@ const OrdersDashboard = () => {
             id,
             name,
             last_name
+            `);
+      const { data: detailData, error: detailError } = await ordersupabase
+          .from("order_details")
+          .select(`
+            id, 
+            quantity,
+            product_id,
+            order_id
             `)
   
       if (ordersError || usersError || consumerError) {
-        console.error("Error fetching data:", ordersError || usersError);
-      } 
-      
-      else {
-        const enrichedOrders = ordersData.map(order => {
-          const consumer = consumerData.find(c => c.id === order.consumer_id);
-          const user = consumer ? usersData.find(u => u.id === consumer.user_id) : null;
-        
-          return {
-            ...order,
-            consumer_name: user ? `${user.name} ${user.last_name}` : "Desconocido",
-            status: order.state_types?.name || "Desconocido"
-          };
-        });
-        
-  
-        setOrders(enrichedOrders);
+        return console.error("Error fetching data:", ordersError || usersError);
       }
+      
+      const enrichedOrders = ordersData.map(order => {
+        const consumer = consumerData.find(c => c.id === order.consumer_id);
+        const user = consumer ? usersData.find(u => u.id === consumer.user_id) : null;
+        
+        return {
+          ...order,
+          consumer_name: user ? `${user.name} ${user.last_name}` : "Desconocido",
+          status: order.state_types?.name || "Desconocido"
+        };
+      });     
+  
+      setOrders(enrichedOrders);
+
   
       setLoading(false);
     };
