44c458461fa067153b283f32c1252c3126e5f7a8
Evert Moreno Serrate <evert23122004@gmail.com>
2025-05-01 12:03:41 -0400

    className en lugar de class
    Para evitar errores de React-Dom

--- OrderNow-React/src/pages/Checkout.jsx
+++ OrderNow-React/src/pages/Checkout.jsx
@@ -26,25 +26,25 @@ const Checkout = () => {
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead>
             <tr className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <th class="px-6 py-3">Producto</th>
-              <th class="px-6 py-3">Cantidad</th>
-              <th class="px-6 py-3">Precio unit.</th>
-              <th class="px-6 py-3">Precio total</th>
+              <th className="px-6 py-3">Producto</th>
+              <th className="px-6 py-3">Cantidad</th>
+              <th className="px-6 py-3">Precio unit.</th>
+              <th className="px-6 py-3">Precio total</th>
             </tr>
           </thead>
           <tbody>
             {products.map((product) => (
-              <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   {product.title}
                 </td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   {product.quantity}
                 </td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   {product.price}
                 </td>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   {product.price * product.quantity}
                 </td>
               </tr>
