a6e459ab41b08030968a3c55186a8d997e378642
RaidenMakio <rmakio96@gmail.com>
2025-05-07 11:47:37 -0400

    Refactorizacion de navbar

--- OrderNow-Dashboard/src/components/layout/Navbar.jsx
+++ OrderNow-Dashboard/src/components/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import NavBarLinkList from '../../subcomponents/NavBarLinkList';
 
 
 const NavBar = () => {
@@ -8,6 +8,14 @@ const NavBar = () => {
   const toggleSearchMenu = () => {
     setIsSearchMenuOpen(!isSearchMenuOpen);
   };
+
+  const links = [
+    { url: '/', text: 'Home' },
+    { url: '/orders', text: 'Ver Pedidos' },
+    { url: '/product/register', text: 'Crear producto' },
+    { url: '/product/1/update', text: 'Modificar producto' },
+    { url: '/status', text: 'Cierre Temporal' },
+  ];
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full">
@@ -74,31 +82,8 @@ const NavBar = () => {
               placeholder="Search..."
             />
           </div>
-          <ul
-            className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-                <Link to="/"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                    Home
-                </Link>
-            </li>
-            <li>
-                <Link to="/orders"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                   Ver Pedidos
-                </Link>
-            </li>
-            <li>
-              <a href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Configuraciones</a>
-            </li>
-            <li>
-                <Link to="/status"
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                   Cierre Temporal
-                </Link>
-            </li>
-          </ul>
+
+          <NavBarLinkList links={links} />
         </div>
       </div>
     </nav>
