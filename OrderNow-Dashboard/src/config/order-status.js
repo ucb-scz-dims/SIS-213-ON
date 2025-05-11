bdd35b29b4dda8f8b86337a5f2dc2cc6591da28e
rafael1199v <123646379+rafael1199v@users.noreply.github.com>
2025-05-10 23:16:41 -0400

    Filtrado por estado de un pedido

--- OrderNow-Dashboard/src/config/order-status.js
+++ OrderNow-Dashboard/src/config/order-status.js
@@ -6,3 +6,12 @@ export const ORDER_STATUS = {
     PREPARING: 5,
     ACCEPTED: 6
 };
+
+export const ORDER_STATUS_NAMES = {
+    [ORDER_STATUS.PENDING]: "Pendiente",
+    [ORDER_STATUS.ON_THE_WAY]: "En Camino",
+    [ORDER_STATUS.DELIVERED]: "Entregado",
+    [ORDER_STATUS.CANCELED]: "Cancelado",
+    [ORDER_STATUS.ACCEPTED]: "Aceptado",
+    [ORDER_STATUS.PREPARING]: "Preparando" 
+}
